c4cd871976f0b8e522fbf77e94e7726452638eb8
nombreUsuario <emal>
2020-09-16 17:01:51 -0500

    boton modificar

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function cambiarNombre(idDiv){
+    let modificar=prompt('Escriba el nuevo nombre')
+    document.getElementById(idDiv).innerHTML=modificar
+}
+
+function cambiarNombre(idDiv){
+    let modificar1=prompt('Escriba el nuevo nombre')
+    document.getElementById(idDiv).innerHTML=modificar1
+}
+
+function cambiarNombre(idDiv){
+    let modificar2=prompt('Escriba el nuevo nombre')
+    document.getElementById(idDiv).innerHTML=modificar2
+}
+
+function cambiarNombre(idDiv){
+    let modificar3=prompt('Escriba el nuevo nombre')
+    document.getElementById(idDiv).innerHTML=modificar3
+}
+
+function cambiarNombre(idDiv){
+    let modificar4=prompt('Escriba el nuevo nombre')
+    document.getElementById(idDiv).innerHTML=modificar4
+}
